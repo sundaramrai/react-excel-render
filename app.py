ee3fb9cfbdfdf41e2a8358cc95253fcd24bdf715
Sundaram Rai <sundaramrai02@gmail.com>
2026-01-01 17:38:51 +0530

    refactor: use constant for column name in ARIMA model

--- app.py
+++ app.py
@@ -9,19 +9,21 @@ input_location = input("Enter file location\n")
 num_predictions = int(input("Number of future predictions\n"))
 output_location = input("Location for new file\n")
 
+COLUMN_NAME = 'Average of OEE VALUE'
+
 data = pd.read_csv(input_location, index_col='Date', parse_dates=True,
                    date_parser=lambda x: pd.to_datetime(x, format='%d-%m-%Y'))
 stepwise_fit = auto_arima(
-    data['Average of OEE VALUE'], trace=True, suppress_warnings=True)
+    data[COLUMN_NAME], trace=True, suppress_warnings=True)
 best_order = stepwise_fit.get_params()['order']
 
-model = ARIMA(data['Average of OEE VALUE'], order=best_order)
+model = ARIMA(data[COLUMN_NAME], order=best_order)
 model = model.fit()
 start = len(data)
 end = start + num_predictions - 1
 
 predicted_values = np.round(model.predict(
-    start=start, end=end).rename('Average of OEE VALUE'), 2)
+    start=start, end=end).rename(COLUMN_NAME), 2)
 predictions_df = pd.concat(
     [data.tail(12), pd.DataFrame(predicted_values)], axis=0)
 predictions_df.index.name = 'Date'
